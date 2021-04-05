76276758b0f707634e89ca680f9e79b8d21eddbf
Manawat <802281@cognizant.com>
2021-04-05 13:43:48 +0530

    Add project files.

---
+++ SuperMarket/Program.cs
@@ -0,0 +1,40 @@
+﻿using SuperMarket.Client;
+using SuperMarket.Logging;
+using SuperMarket.Repository;
+using SuperMarket.Service;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace SuperMarket
+{    
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your customer id ->");
+            int customerId = Int32.Parse(Console.ReadLine());
+            while(customerId.ToString() == null)
+            {
+                Console.WriteLine("Customer id cannot be empty.");
+                customerId = Int32.Parse(Console.ReadLine());
+            }
+            var stockService = new StockService(new StockRepository());
+            var discountService = new DiscountService(new DiscountRepository());
+            var paymentGateWay = new PaymentGatewayClient(new HttpClient());
+            var logger = new Logger();
+            List<string> items = new List<string>()
+            {
+                "Breads",
+                "Milk",
+                "Cheese",
+                "Butter",
+                "Biscuits",
+                "Dryfruits"
+            };
+            ShoppingService shopping = new ShoppingService(customerId, stockService, discountService, paymentGateWay, logger);
+            var count = shopping.BuyItems(items);
+            Console.WriteLine($"Shopping done for {count} products ");
+        }
+    }
+}
